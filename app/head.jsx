20e82e4b5fe295628d9727182cc8431661247ce8
ConnorWotkowicz <wottkowicz@gmail.com>
2025-06-23 09:47:29 -0400

    Add Head

---
+++ app/head.jsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>SoundPiece</title>
+      <meta name="theme-color" content="#000000" />
+
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="icon" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" sizes="512x512" href="/android-chrome-512x512.png" />
+    </>
+  );
+}
